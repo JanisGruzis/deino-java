51517cfdd56e827acdcad970f593ce78c4606927
roberts <roberts.dargis@gmail.com>
2015-04-09 00:09:11 +0300

    csv output

--- src/com/deino/Main.java
+++ src/com/deino/Main.java
@@ -1,9 +1,43 @@
 package com.deino;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.commons.lang3.StringEscapeUtils;
+
+import javax.swing.text.DateFormatter;
 
 public class Main {
+	
+	public static void saveToCsv(HashMap<String,Article> articles, File f) throws Exception
+	{
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(f)));
+		Article a;
+		SimpleDateFormat date_format=new SimpleDateFormat("yyyy/MM/dd");
+		
+		out.write("date,title,category,predefined_category,category,description,url,img_url\n");
+		
+		for (Map.Entry<String, Article> entry : articles.entrySet())
+		{
+			a=entry.getValue();
+			
+			out.write(String.format("%s,",date_format.format(a.getPublication_date())));
+			out.write(String.format("%s,",StringEscapeUtils.escapeCsv(a.getTitle())));
+			out.write(String.format("%s,",StringEscapeUtils.escapeCsv(a.getCategory())));
+			out.write(String.format("%s,",StringEscapeUtils.escapeCsv(a.getPredefined_category())));
+			out.write(String.format("%s,",StringEscapeUtils.escapeCsv(a.getCategory())));
+			out.write(String.format("%s,",StringEscapeUtils.escapeCsv(a.getDescription())));
+			out.write(String.format("%s,",StringEscapeUtils.escapeCsv(a.getURL())));
+			out.write(String.format("%s\n",StringEscapeUtils.escapeCsv(a.getImg_url())));
+		}
+		
+		out.close();
+	}
 
     public static void main(String[] args) {
 
@@ -21,6 +55,12 @@ public class Main {
             System.out.println("=================================");
             System.out.println();
         }
+        
+        try {
+			Main.saveToCsv(result, new File("articles.csv"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
     }
 
 
